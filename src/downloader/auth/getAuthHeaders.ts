fb0d9e2539378b38468ff29152d66d38572d3434
Sergio Almela <sergioalmela@gmail.com>
2023-11-01 23:02:08 +0100

    Fix connection with twitch

--- src/downloader/auth/getAuthHeaders.ts
+++ src/downloader/auth/getAuthHeaders.ts
@@ -1,15 +1,11 @@
 type AuthHeaders = {
-    headers: {
-        'Content-Type': string
-        'Client-ID': string
-    }
+  'Content-Type': string
+  'Client-ID': string
 }
 
 export const getAuthHeaders = (): AuthHeaders => {
-    return {
-        headers: {
-            'Content-Type': 'text/plain;charset=UTF-8',
-            'Client-ID': 'kimne78kx3ncx6brgo4mv6wki5h1ko'
-        }
-    }
+  return {
+    'Content-Type': 'text/plain;charset=UTF-8',
+    'Client-ID': 'kimne78kx3ncx6brgo4mv6wki5h1ko'
+  }
 }
