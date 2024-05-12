ebcc3099542c63bdc08b3a11bc87d8561ea0438b
Sergio Almela <sergioalmela@gmail.com>
2024-05-12 21:50:14 +0200

    Detect translation in the frontend at startup

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,46 @@ import i18next from 'i18next'
 import { I18nextProvider, initReactI18next } from 'react-i18next'
 import enTranslations from './locales/en.json'
 import esTranslations from './locales/es.json'
+import useInit from './hooks/useInit.ts'
+import { invoke } from '@tauri-apps/api/tauri'
+import { useEffect, useState } from 'preact/hooks'
 
-i18next.use(initReactI18next).init({
-  resources: {
-    en: {
-      translation: enTranslations
-    },
-    es: {
-      translation: esTranslations
-    }
-  },
-  lng: 'es',
-  fallbackLng: 'en',
-  interpolation: {
-    escapeValue: false
-  }
-})
+type Config = {
+  theme: string
+  language: string
+  download_folder: string
+  open_on_download: string
+}
 
 function App() {
+  const [config, setConfig] = useState<Config | null>(null)
+
+  i18next.use(initReactI18next).init({
+    resources: {
+      en: {
+        translation: enTranslations
+      },
+      es: {
+        translation: esTranslations
+      }
+    },
+    lng: 'en',
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false
+    }
+  })
+
+  useInit(async () => {
+    setConfig(await invoke('get_preferences'))
+  })
+
+  useEffect(() => {
+    if (config) {
+      i18next.changeLanguage(config.language)
+    }
+  }, [config])
+
   return (
     <I18nextProvider i18n={i18next}>
       <ThemeProvider>
